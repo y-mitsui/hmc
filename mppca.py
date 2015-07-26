a9607b766d428e0f3522bf29d3eb8f5da9ce07c2
Y-Mitsui <dyfomg@gmail.com>
2015-07-27 02:11:05 +0900

    commit

--- mppca.py
+++ mppca.py
@@ -1,3 +1,26 @@
+import numpy
 
-class ppca():
+def ppca(sample,iter=100):
+    num_dimention = sample.shape[1]
+    
+    weights = numpy.nrand()
+    latent_variables = numpy.nrand()
+    sigma2 = 1.
+    for _ range(iter):
+        M = numpy.dot(weights,weights.T) + sigma2*numpy.matrix(numpy.identity(num_dimention))
+        a = b = 0.
+        for x in sample:
+            diff = x-x_mean
+            mean_latent_variables = numpy.dot(numpy.dot(M.I,weights.T),diff)
+            mean_latent_variables2 = sigma2*M.I+numpy.dot(mean_latent_variables,mean_latent_variables.T)
+            a += numpy.dot(diff,mean_latent_variables.T)
+            b += mean_latent_variables2
+        weights = numpy.dot(a,b.T)
+        for x in sample:
+            diff = x-x_mean
+            mean_latent_variables = numpy.dot(numpy.dot(M.I,weights.T),diff)
+            mean_latent_variables2 = sigma2*M.I+numpy.dot(mean_latent_variables,mean_latent_variables.T)
+            tr_a=sum(numpy.diag(numpy.dot(numpy.dot(mean_latent_variables2,weights),weights)))
+            c += diff*diff-numpy.dot((2*mean_latent_variables).T,weights.T)*diff+tr_a
+        sigma2 = c/(sample.shape[0]*sample.shape[1])
     

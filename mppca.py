c6859d661ff67d1dd9c98dd3d09d41e714cdd929
Y-Mitsui <dyfomg@gmail.com>
2015-07-28 00:41:04 +0900

    commit

--- mppca.py
+++ mppca.py
@@ -9,63 +9,53 @@ import math
 import random
 from matplotlib import pyplot as plt
 
-def ppca(sample,n_components=2,iter=100):
+def ppca(sample,n_components=2,iter=1000):
     sample=numpy.matrix(sample)
-    print sample.shape
     num_dimention = sample.shape[1]
     x_mean = numpy.average(sample,axis=0)
-    weights = numpy.random.randn(num_dimention,n_components)
+    latest_weights = weights = numpy.matrix(numpy.random.randn(num_dimention,n_components)*2.0+1.0)
     latent_variables = numpy.random.randn(n_components)
-    sigma2 = 10.
+    latest_sigma2 = sigma2 = 10.
     for index in range(iter):
-        M = numpy.dot(weights.T,weights) + float(sigma2)*numpy.matrix(numpy.identity(n_components))
-        print M
+        M = weights.T*weights + float(sigma2)*numpy.matrix(numpy.identity(n_components))
         M_inv = numpy.linalg.inv(M)
         # E step
-        mean_latent_variables = [numpy.dot(numpy.dot(M_inv,weights.T),(x-x_mean).T) for x in sample]
-        mean_latent_variables2 = [float(sigma2)*M_inv+numpy.dot(mean_latent_variables[i],mean_latent_variables[i].T) for i,x in enumerate(sample)]
+        mean_latent_variables = [M_inv*weights.T*(x-x_mean).T for x in sample]
+        mean_latent_variables2 = [float(sigma2)*M_inv+mean_latent_variables[i]*mean_latent_variables[i].T for i,x in enumerate(sample)]
+        
         # M step
         a = numpy.zeros([num_dimention,n_components])
         b = numpy.zeros([n_components,n_components])
         for i,x in enumerate(sample):
             diff = (x-x_mean).T
-            a = a + numpy.dot(diff,mean_latent_variables[i].T)
+            a = a + diff*mean_latent_variables[i].T
             b = b + mean_latent_variables2[i]
-
-        weights = numpy.dot(a,numpy.linalg.inv(b))
-
+        weights = a*numpy.linalg.inv(b)
         c = 0.
         for i,x in enumerate(sample):
             diff = (x-x_mean).T
-            tr_a = sum(numpy.diag(numpy.dot(numpy.dot(mean_latent_variables2[i],weights.T),weights)))
-            tmp = numpy.dot(2*mean_latent_variables[i].T,weights.T)
-            norm2 = math.sqrt((numpy.array(x-x_mean)*numpy.array(x-x_mean)).sum())
-            c += norm2-numpy.dot(tmp,diff)+tr_a
+            tr_a = sum(numpy.diag(mean_latent_variables2[i]*weights.T*weights))
+            tmp = 2*mean_latent_variables[i].T*weights.T*diff
+            norm2 = float(diff.T*diff)
+            c += norm2-tmp+tr_a
         sigma2 = c/(sample.shape[0]*sample.shape[1])
         if index % 100 == 0:
             print "weights:\n{0}".format(weights)
+        
+        epcilon_w = numpy.fabs(latest_weights - weights).max()
+        epcilon_s = abs(latest_sigma2 - sigma2)
+        if epcilon_w < 1e-5 and epcilon_s < 1e-5 :
+            break;
+        latest_weights = weights
+        latest_sigma2 = sigma2
     return [weights,sigma2,x_mean]
 
 iris = datasets.load_iris()
-#alist = iris.data.tolist()
-#blist = iris.target.tolist()
-#clist = [0. for _ in range(len(alist))]
-#dlist = [0. for _ in range(len(alist))]
-#for i in range(len(alist)):
-#    idx = random.randint(0,len(alist)-1)
-#    clist[idx]=alist[len(alist)-idx]
-#    alist[i] = 
-#random.shuffle(alist)
-#iris.data = numpy.array(alist)
 [weights,sigma2,x_mean] = ppca(iris.data)
-print sigma2
-#sys.exit(1)
 m = tr(weights).dot(weights) + float(sigma2) * numpy.eye(weights.shape[1])
 m = numpy.linalg.inv(m)
 targets = iris.target
 for i,x in enumerate(numpy.matrix(iris.data)):
     new_sample = m.dot(tr(weights)).dot((x - x_mean).T)
-#    print new_sample 
-#    new_sample=numpy.array(numpy.dot(numpy.matrix(x),weights))
     plt.scatter(new_sample[0][0],new_sample[1][0],c=['r','g','b'][targets[i]])
 plt.show()

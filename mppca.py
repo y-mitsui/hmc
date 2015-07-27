0a2ca5bea786e7a37f1794b2ca092951d17aab99
Y-Mitsui <dyfomg@gmail.com>
2015-07-27 22:47:49 +0900

    commit

--- mppca.py
+++ mppca.py
@@ -4,18 +4,22 @@ from sklearn import datasets
 import numpy
 import numpy.linalg
 import numpy.random
+from numpy import transpose as tr
 import math
+import random
+from matplotlib import pyplot as plt
 
-def ppca(sample,n_components=2,iter=4000):
+def ppca(sample,n_components=2,iter=100):
     sample=numpy.matrix(sample)
     print sample.shape
     num_dimention = sample.shape[1]
     x_mean = numpy.average(sample,axis=0)
     weights = numpy.random.randn(num_dimention,n_components)
     latent_variables = numpy.random.randn(n_components)
-    sigma2 = 1.
+    sigma2 = 10.
     for index in range(iter):
         M = numpy.dot(weights.T,weights) + float(sigma2)*numpy.matrix(numpy.identity(n_components))
+        print M
         M_inv = numpy.linalg.inv(M)
         # E step
         mean_latent_variables = [numpy.dot(numpy.dot(M_inv,weights.T),(x-x_mean).T) for x in sample]
@@ -27,6 +31,7 @@ def ppca(sample,n_components=2,iter=4000):
             diff = (x-x_mean).T
             a = a + numpy.dot(diff,mean_latent_variables[i].T)
             b = b + mean_latent_variables2[i]
+
         weights = numpy.dot(a,numpy.linalg.inv(b))
 
         c = 0.
@@ -39,9 +44,28 @@ def ppca(sample,n_components=2,iter=4000):
         sigma2 = c/(sample.shape[0]*sample.shape[1])
         if index % 100 == 0:
             print "weights:\n{0}".format(weights)
-    return weights
+    return [weights,sigma2,x_mean]
 
 iris = datasets.load_iris()
-weights = ppca(iris.data)
-for x in iris.data:
-    fa = numpy.dot(iris.data,weights)
+#alist = iris.data.tolist()
+#blist = iris.target.tolist()
+#clist = [0. for _ in range(len(alist))]
+#dlist = [0. for _ in range(len(alist))]
+#for i in range(len(alist)):
+#    idx = random.randint(0,len(alist)-1)
+#    clist[idx]=alist[len(alist)-idx]
+#    alist[i] = 
+#random.shuffle(alist)
+#iris.data = numpy.array(alist)
+[weights,sigma2,x_mean] = ppca(iris.data)
+print sigma2
+#sys.exit(1)
+m = tr(weights).dot(weights) + float(sigma2) * numpy.eye(weights.shape[1])
+m = numpy.linalg.inv(m)
+targets = iris.target
+for i,x in enumerate(numpy.matrix(iris.data)):
+    new_sample = m.dot(tr(weights)).dot((x - x_mean).T)
+#    print new_sample 
+#    new_sample=numpy.array(numpy.dot(numpy.matrix(x),weights))
+    plt.scatter(new_sample[0][0],new_sample[1][0],c=['r','g','b'][targets[i]])
+plt.show()
--- pca_test.py
+++ pca_test.py
@@ -3,14 +3,15 @@ sys.path.append("/usr/local/lib/python2.7/site-packages/")
 from sklearn import datasets
 from sklearn.decomposition import PCA, KernelPCA
 from matplotlib import pyplot as plt
-
+import numpy
+import random
 
 iris = datasets.load_iris()
 targets = iris.target
-kpca = KernelPCA(n_components=2,kernel="linear", fit_inverse_transform=True, gamma=10)
-#kpca = PCA(n_components=2,kernel="rbf", fit_inverse_transform=True, gamma=10)
+
+#kpca = KernelPCA(n_components=2,kernel="linear", fit_inverse_transform=True, gamma=10)
+kpca = PCA(n_components=2)
 X_kpca = kpca.fit_transform(iris.data)
-print X_kpca
 for c,data in zip(targets,X_kpca):
     plt.scatter(data[0],data[1],c=['r','g','b'][c])
 plt.show()
